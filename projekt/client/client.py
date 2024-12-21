ed2491ba437ee85b79bd257c3f991e9c813bf700
fryniewi <01178583@pw.edu.pl>
2024-12-21 22:00:35 +0100

    seperated ui from logic

--- projekt/client/client.py
+++ projekt/client/client.py
@@ -16,67 +16,16 @@ class Client:
         self.public_key = generate_key()
         self.stop_event = threading.Event()
 
-    def client_loop(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.socket:
-            print("Establishing connection...")
-            self.socket.connect((self.host, self.port))
-            self.client_hello()
-            print("Key exchange successful. Enter your message below.")
-            listener_thread = threading.Thread(
-                target=self.listen_for_server_messages, daemon=True
-            )
-            listener_thread.start()
-
-            while not self.stop_event.is_set():
-                message = input()
-
-                binary_stream = io.BytesIO()
-                encoded_message = encrypt(message, self.session_key).encode("ascii")
-                mac = generate_mac(encoded_message, self.session_key)
-                binary_stream.write(mac)
-                binary_stream.write(encoded_message)
-                binary_stream.seek(0)
-                try:
-                    self.socket.sendall(binary_stream.read())
-                    if message.lower() == "exit":
-                        print("Client closing connection...")
-                        break
-                except socket.error:
-                    print("Error sending message. Connection may have been closed.")
-                    break
-
-            self.stop_event.set()
-            listener_thread.join()
-            print("Client loop exiting.")
-
-    def listen_for_server_messages(self):
-        while not self.stop_event.is_set():
-            try:
-                data = self.socket.recv(self.buf_size)
-                if not data:
-                    print("Server closed connection.")
-                    self.stop_event.set()
-                    break
-
-                response = decrypt(data.decode("ascii"), self.session_key)
-                print(f"\nServer response: {response}")
-
-                if response.lower() == "exit":
-                    print("Server closed connection. Press enter to exit.")
-                    self.stop_event.set()
-                    break
-
-            except socket.error:
-                print("Error receiving message from server. Press enter to exit.")
-                self.stop_event.set()
-                break
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+        self.client_hello()
 
     def client_hello(self):
         secret_key = generate_key()
         self.socket.sendall(str.encode(self.public_key))
 
         server_key = self.socket.recv(self.buf_size).decode("ascii")
-
         common_key = server_key + self.public_key
         common_encoded = encrypt(common_key, secret_key)
         self.socket.sendall(str.encode(common_encoded))
@@ -84,9 +33,83 @@ class Client:
         encoded_server = self.socket.recv(self.buf_size).decode("ascii")
         self.session_key = encrypt(encoded_server, secret_key)
 
+    def send_message(self, message):
+        encoded_message = encrypt(message, self.session_key).encode("ascii")
+        mac = generate_mac(encoded_message, self.session_key)
+
+        binary_stream = io.BytesIO()
+        binary_stream.write(mac)
+        binary_stream.write(encoded_message)
+        binary_stream.seek(0)
+
+        self.socket.sendall(binary_stream.read())
+
+    def receive_message(self):
+        data = self.socket.recv(self.buf_size)
+        if not data:
+            raise ConnectionError("Server closed connection.")
+        response = decrypt(data.decode("ascii"), self.session_key)
+        return response
+
+    def close(self):
+        self.stop_event.set()
+        self.socket.close()
+
+
+class ClientUI:
+    def __init__(self, client: Client):
+        self.client = client
+
+    def run(self):
+        print("Establishing connection...")
+        try:
+            self.client.connect()
+            print("Key exchange successful. Enter your messages below.")
+        except Exception as e:
+            print(f"Failed to connect: {e}")
+            return
+
+        listen_thread = threading.Thread(
+            target=self.listen_for_server_messages, daemon=True
+        )
+        listen_thread.start()
+
+        while not self.client.stop_event.is_set():
+            try:
+                message = input()
+                self.client.send_message(message)
+                if message.lower() == "exit":
+                    self.is_exiting = True
+                    print("Client closing connection...")
+                    break
+            except Exception as e:
+                print(f"Error: {e}")
+                break
+
+        self.client.stop_event.set()
+        listen_thread.join()
+        print("Client UI exiting.")
+
+    def listen_for_server_messages(self):
+        while not self.client.stop_event.is_set():
+            try:
+                response = self.client.receive_message()
+                print(f"\nServer response: {response}")
+                if response.lower() == "exit":
+                    print("Server closed connection.")
+                    if not self.is_exiting:
+                        print("Press enter to exit.")
+                    self.client.stop_event.set()
+                    break
+            except Exception as e:
+                print(f"Press enter to exit.")
+                self.client.stop_event.set()
+                break
+
 
 if __name__ == "__main__":
     host = sys.argv[1]
     port = int(sys.argv[2])
-    client = Client(host, port)
-    client.client_loop()
+    client_logic = Client(host, port)
+    client_ui = ClientUI(client_logic)
+    client_ui.run()
--- projekt/server/server.py
+++ projekt/server/server.py
@@ -1,11 +1,11 @@
 import socket
 import sys
-import time
 import threading
+import time
 from utils import generate_key, encrypt, decrypt, generate_mac
 from concurrent.futures import ThreadPoolExecutor
+from multimethod import multimethod
 
-HOST = "127.0.0.1"
 BUF_SIZE = 512
 BACKLOG = 10
 
@@ -20,6 +20,73 @@ class Server:
         self.connected_clients = []
         self.lock = threading.Lock()
         self.running = True
+        self.session_key = None
+        self.socket = None
+
+    def start(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(self.backlog)
+        print(f"Server started on {self.host}:{self.port}")
+
+    def stop(self):
+        self.running = False
+        with self.lock:
+            for conn, _ in self.connected_clients:
+                self.disconnect_client(conn)
+        if self.socket:
+            self.socket.close()
+        print("Server stopped.")
+
+    def accept_clients(self):
+        with ThreadPoolExecutor(max_workers=self.backlog) as executor:
+            while self.running:
+                try:
+                    conn, addr = self.socket.accept()
+                    with self.lock:
+                        self.connected_clients.append((conn, addr))
+                    executor.submit(self.handle_client, conn, addr)
+                except Exception as e:
+                    print(f"Error accepting clients: {e}")
+                    break
+
+    def handle_client(self, conn, addr):
+        with conn:
+            print(f"New connection from {addr}")
+            self.server_hello(conn)
+            while True:
+                data = conn.recv(self.buf_size)
+                if not data:
+                    break
+                mac, encrypted_msg = self.separate_data(data)
+                if generate_mac(encrypted_msg.encode("ascii"), self.session_key) != mac:
+                    print(f"MAC verification failed for client {addr}")
+                    response = "ERROR: MAC verification failed"
+                else:
+                    decrypted_msg = decrypt(encrypted_msg, self.session_key)
+                    print(f"Message from {addr}: {decrypted_msg}")
+                    if decrypted_msg == "exit":
+                        response = encrypt("exit", self.session_key)
+                        print(f"Connection closed with {addr}")
+                        conn.sendall(str.encode(response))
+                        break
+                    else:
+                        response = encrypt(f"ACK: {addr} received", self.session_key)
+                conn.sendall(str.encode(response))
+            self.disconnect_client(conn, addr)
+
+    def disconnect_client(self, conn, addr):
+        conn.close()
+        with self.lock:
+            self.connected_clients.remove((conn, addr))
+
+    def disconnect_client_by_index(self, index):
+        response = encrypt("exit", self.session_key)
+        self.connected_clients[index][0].sendall(str.encode(response))
+        time.sleep(1)
+        self.connected_clients[index][0].close()
+        with self.lock:
+            self.connected_clients.pop(index)
 
     def server_hello(self, conn):
         secret_key = generate_key()
@@ -31,135 +98,74 @@ class Server:
         encoded_client = conn.recv(self.buf_size).decode("ascii")
         self.session_key = encrypt(encoded_client, secret_key)
 
-    def server_loop(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.socket:
-            print(f"Server started with hostname {self.host} on port {self.port}")
-            self.socket.bind((self.host, self.port))
-            self.socket.listen(self.backlog)
-            self.communicate()
-
-    def communicate(self):
-        with ThreadPoolExecutor(max_workers=self.backlog) as executor:
-            while self.running:
-                # accept connections
-                conn, addr = self.socket.accept()
-                with self.lock:
-                    self.connected_clients.append((conn, addr))
-                executor.submit(self.handle_client, conn, addr)
-
-    def handle_client(self, conn, addr):
-        time.sleep(0.5)
-        thread_name = threading.current_thread().name
-        thread_id = threading.get_ident()
-        with conn:
-            print(
-                f"Connect from {addr} handled by thread with name {thread_name} and id {thread_id}"
-            )
-            self.server_hello(conn)
-            while True:
-                data = conn.recv(self.buf_size)
-                if not data:
-                    break
-                mac, encrypted_msg = self.seperate_data(data)
-                if generate_mac(encrypted_msg.encode("ascii"), self.session_key) != mac:
-                    print(f"MAC verification failed for message from {addr}")
-                    response = "ERROR: MAC verification failed"
-                else:
-                    decrypted_msg = decrypt(encrypted_msg, self.session_key)
-                    print(f"Received message from {addr}: {decrypted_msg}")
-                    if decrypted_msg == "exit":
-                        response = encrypt(
-                            f"exit",
-                            self.session_key,
-                        )
-                        break
-                    else:
-                        response = encrypt(
-                            f"ACK: Communication successful with {addr}",
-                            self.session_key,
-                        )
-                conn.sendall(str.encode(response))
-        conn.close()
-        with self.lock:
-            self.connected_clients.remove((conn, addr))
-        print(f"Connection closed by client {addr}")
-
-    def seperate_data(self, data):
+    def separate_data(self, data):
         mac = data[:32]
         encrypted_msg = data[32:].decode("ascii")
         return mac, encrypted_msg
 
-    def print_connected(self):
-        if not self.connected_clients:
-            print("No clients connected.")
+    def get_connected_clients(self):
+        return self.connected_clients
+
+
+class ServerUI:
+    def __init__(self, server: Server):
+        self.server = server
+
+    def display_menu(self):
+        print("\nServer Menu:")
+        print("1. Manage connections")
+        print("2. Stop server")
+
+    def run(self):
+        try:
+            self.server.start()
+            server_thread = threading.Thread(
+                target=self.server.accept_clients, daemon=True
+            )
+            server_thread.start()
+
+            while self.server.running:
+                self.display_menu()
+                choice = input("Choose an option: ").strip()
+                if choice == "1":
+                    self.disconnect_client()
+                elif choice == "2":
+                    print("Stopping server...")
+                    self.server.stop()
+                    break
+                else:
+                    print("Invalid choice. Please try again.")
+        except Exception as e:
+            print(f"Error running server UI: {e}")
+
+    def show_connected_clients(self):
+        clients = self.server.get_connected_clients()
+        if not clients:
+            print("No clients to disconnect.")
+            return
         else:
             print("Connected clients:")
-            for idx, client in enumerate(self.connected_clients):
-                print(f"{idx}. {client[1]}")
+            for index, params in enumerate(clients):
+                print(f"{index}: {params[1]}")
 
-    def manage_connections(self):
-        self.print_connected()
-        if not self.connected_clients:
-            return
+    def disconnect_client(self):
+        clients = self.server.get_connected_clients()
+        self.show_connected_clients()
         try:
-            chosen = int(
-                input("Enter the index of the client you want to disconnect: ")
-            )
-            if 0 <= chosen < len(self.connected_clients):
-                self.close_client_connection(self.connected_clients[chosen][0], chosen)
-                print(f"Disconnected client {chosen}")
-            else:
-                print("Invalid client index")
+            if len(clients) > 0:
+                index = int(input("Enter the client index to disconnect: ").strip())
+                if 0 <= index < len(clients):
+                    self.server.disconnect_client_by_index(index)
+                    print(f"Client {index} disconnected.")
+                else:
+                    print("Invalid index.")
         except ValueError:
-            print("Invalid input. Please enter a valid number.")
-
-    def close_server(self):
-        self.running = False
-        with self.lock:
-            for i, conn, _ in enumerate(self.connected_clients):
-                self.close_client_connection(conn, i)
-        print("Server is shutting down.")
-
-    def close_client_connection(self, conn, chosen):
-        with self.lock:
-            self.connected_clients.pop(chosen)
-        response = encrypt(
-            "exit",
-            self.session_key,
-        )
-        conn.sendall(str.encode(response))
-        conn.close()
-
-
-def display_menu():
-    print("\nServer Menu:")
-    print("1. Connection logs")
-    print("2. Manage connections")
-    print("3. Close server")
-
-
-def terminal_menu(server):
-    while True:
-        display_menu()
-        choice = input("Select an option: ")
-        if choice == "1":
-            pass
-        elif choice == "2":
-            server.manage_connections()
-        elif choice == "3":
-            print("Exiting program...")
-            server.close_server()
-            break
-        else:
-            print("Invalid choice. Please try again.")
+            print("Invalid input. Please enter a number.")
 
 
 if __name__ == "__main__":
     host = sys.argv[1]
     port = int(sys.argv[2])
-    server = Server(host, port)
-
-    server_thread = threading.Thread(target=server.server_loop)
-    server_thread.daemon = True
-    server_thread.start()
-    terminal_menu(server)
+    server_logic = Server(host, port)
+    server_ui = ServerUI(server_logic)
+    server_ui.run()
